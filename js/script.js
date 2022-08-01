ff57dc613c45d761c4291a2ab47aadeff0bc7505
André Bianchi <dcbianchi15@gmail.com>
2022-08-01 09:33:43 -0300

    Refactory

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
-console.log("Inicio da aplicação");
-
-function RevelarResposta() {
+/* - Java SandCodebox */
+function revelarResposta() {
   var resposta = document.querySelector("#resposta");
   resposta.classList.toggle("blur");
 }
 
-function ProximaPergunta(ProximaPergunta) {
-  var pergunta = document.querySelector("#CardContainer");
-  pergunta.innerHTML="";
+function proximaPergunta(proximaPergunta) {
+  var pergunta = document.querySelector("#cardContainer");
+  pergunta.innerHTML = "";
 
-  var CardDiv = document.createElement("div");
-  CardDiv.classList.add("Card");
-  CardDiv.innerHTML=`
-  <div class="Card">
-    <div class="QuestionCard Centralizar">
-      <div class="HeaderCard">
-        <h2>O que é HTML?</h2>
-      </div>
-    </div>
-    <div id="resposta" class="AnswerCard blur">
-      <p>A linguagem de Marcação de Hipertexto (HTML) é uma linguagem de computador que compõe a maior parte das páginas da internet e dos aplicativo online. </p>
-    </div>
-  </div>
-`;
+  var cardDiv = document.createElement("div");
 
-  pergunta.appendChild(CardDiv);
+  cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
+
+  console.log(proximaPergunta);
+
+  cardDiv.innerHTML = `
+    <div class="card-cabecalho centralizar">
+      <h1 class="card-titulo">O que é ${proximaPergunta.title}?</h1>
+    </div>
+
+    <div id="resposta" class="card-conteudo blur">
+      <p>
+        ${proximaPergunta.description}
+      </p>
+    </div>
+  `;
+  pergunta.appendChild(cardDiv);
 }
-/* Link do API de Perguntas
-RandomAPI:
-  https://flash.quickstaart.com/random */
 
 function buscarInformacao() {
   fetch("https://flash.quickstaart.com/random")
-  .then(function (resultado) {
-    return resultado.json();
-  })
-  .then(funtion (resultadoJson){
-    ProximaPergunta(resultadoJason);
-  });
+    .then(function (resultado) {
+      return resultado.json();
+    })
+    .then(function (resultadoJson) {
+      proximaPergunta(resultadoJson);
+    });
 }
+
+window.addEventListener("load", buscarInformacao);
